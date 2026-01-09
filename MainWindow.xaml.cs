df7f312c833606cb7eb2228c849c67bc8e083bd4
Thomas Radigk <t.radigk@outlook.de>
2026-01-09 10:44:43 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Documents;
+
+namespace App1;
+
+/// <summary>
+/// An empty window that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Hyperlink_OnClick(Hyperlink sender, HyperlinkClickEventArgs args)
+    {
+        var flyoutShowOptions = new FlyoutShowOptions()
+        {
+            Placement = FlyoutPlacementMode.Auto
+        };
+        Debug.Assert(sender.GetType() == typeof(DependencyObject));
+
+        var flyout = new Flyout()
+        {
+            Content = new TextBlock()
+            {
+                Text = "Hello, World!"
+            }
+        };
+        flyout.ShowAt(sender, flyoutShowOptions);
+    }
+}
